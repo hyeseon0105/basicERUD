35ad7b8313b0355e4ba1848a0203229d4742d489
hyeseon0105 <hyesun0536@gmail.com>
2025-02-18 10:43:46 +0900

    #2. CRUD완성

--- src/main/java/com/my/basicCRUD/service/MemberService.java
+++ src/main/java/com/my/basicCRUD/service/MemberService.java
@@ -5,6 +5,7 @@ import com.my.basicCRUD.emtty.Member;
 import com.my.basicCRUD.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,5 +30,37 @@ public class MemberService {
 //            dtoList.add(dto);
 //        }
 //        return dtoList;
+    }
+
+    public void saveMember(MemberDto dto) {
+//        dto->entity 변환
+        Member member = MemberDto.fromDto(dto);
+//        저장요청
+        memberRepository.save(member);
+
+    }
+
+    public void deleteById(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    public MemberDto findByID(Long id) {
+        //id로db에서 검색하기
+        Member member = memberRepository.findById(id).orElse(null);
+//        dto로 변환
+        if (!ObjectUtils.isEmpty(member)) {
+            return MemberDto.fromEntity(member);
+        } else {
+            return null;
+        }
+
+    }
+
+    public void updateMember(MemberDto dto) {
+        //dto를 entity
+        Member member = MemberDto.fromDto(dto);
+        //수정처리
+        //save():해당 Id가 존재하면 수정, 없으면 입력
+                memberRepository.save(member);
     }
 }
